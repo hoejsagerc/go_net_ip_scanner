85ae5d6e8ad6600bb2195f2e7f2ad73d814d4ca2
Christian Højsager <chho@edgemo.com>
2024-06-22 20:26:50 +0200

    command clean up

--- goscan/cmd/root.go
+++ goscan/cmd/root.go
@@ -12,17 +12,11 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "go_net_ip_scanner",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:   "goscan",
+	Short: "A tcp network scanner written in go",
+	Long: `A network scanner for scanning tcp ports and locating devices on either
+	local network or remote networks. The scanner can be used to scan a single or a range of
+	ip addresses and ports.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -39,14 +33,6 @@ func addSubCommandPalletes() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go_net_ip_scanner.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	addSubCommandPalletes()
